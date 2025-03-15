aad1b06d120751e7e2bea350a0c4fe3abf2d0525
aliothchen <cyh627@sjtu.edu.cn>
2025-03-15 15:46:46 +0800

    add Exposing-the-Deception

---
+++ Exposing-the-Deception/train.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+python training.py  --name FFPP2503021626                                      \
+                    --gpu_num 0,1                                                   \
+                    --model resnet                    \
+                    --epoch 20                                                      \
+                    --weight_decay 1e-6                                             \
+                    --lr 1e-3                                                       \
+                    --bs 256                                                        \
+                    --test_bs 1000                                                  \
+                    --num_workers 12                                                \
+                    --size 224                                                      \
+                    --dataset FF++_c23    \
+                    --mixup True                                                    \
+                    --alpha 0.5                                                     \
+                    --lil_loss True                             \
+                    --gil_loss True                             \
+                    --temperature 1.5                           \
+                    --mi_calculator kl                          \
+                    --balance_loss_method auto,hyper            \
+                    --scales [1,2,10]                           \
+                    --num_LIBs 4                                \
+                    --test False                                \
+                    --save_model True                           \
+                    --save_path output                          \
